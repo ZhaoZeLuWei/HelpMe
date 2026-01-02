9bc9bd32e21f2d1375e3d61c446fdaf755b1f839
Zewei Xia 夏泽苇 <xiazeweibobby@gmail.com>
2026-01-02 17:25:08 +0800

    2 rooms for test use
    2 rooms (simple data structure)

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -40,8 +40,10 @@ export class Tab3Page {
 
   //add room_id 1-2 注意这里的数据结构全部为模拟作用，并非实际情况，用户聊天数据库+ mysql对接待设计
   chatRooms = [
-    { roomId:'room_001',name: 'User1', time: '13:29', lastMsg: 'Hello World', count: 1, avatar: 'assets/icon/user.svg' },
-    { roomId:'room_001',name: 'User2', time: '14:30', lastMsg: '你好', count: 2, avatar: 'assets/icon/user.svg' }
+    { roomId:'room_001',name: 'User1-Room1', time: '13:29', lastMsg: 'Hello World', count: 1, avatar: 'assets/icon/user.svg' },
+    { roomId:'room_001',name: 'User2-Room1', time: '14:30', lastMsg: '你好', count: 2, avatar: 'assets/icon/user.svg' },
+    { roomId:'room_002',name: 'User1-Room2', time: '13:29', lastMsg: 'Hello World', count: 1, avatar: 'assets/icon/user.svg' },
+    { roomId:'room_002',name: 'User2-Room2', time: '14:30', lastMsg: '你好', count: 2, avatar: 'assets/icon/user.svg' },
   ];
 
   //go to the chat with router
