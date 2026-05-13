461c4d7e0a8d37eb065f03bab5e1fd9a5dc667af
JingyuZhang <j.zhang.73@student.scu.edu.au>
2026-05-14 00:08:04 +0800

    Add apiBase for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  apiBase: 'http://localhost:3000',
   //apiBase: 'https://api.helpme.com', // 生产环境应使用实际 API 地址
   // 阿里云图形验证码 H5 SDK（需下载 ct4.js 放到 src/assets/ 目录）
   captchaScriptUrl: '/assets/ct4.js',
